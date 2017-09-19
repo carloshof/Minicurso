8677ea1c93452d905660843e19c51e97b925b318
carloshof <carloshof12@gmail.com>
2017-09-19 15:54:03 -0400

    backEnd

--- oi.py
+++ oi.py
@@ -5,6 +5,14 @@ class User(me.Document):
 	name = me.StringField()
 	email = me.StringField()
 
+	def to_dict(self):
+		return{
+			'id':str(self.id),
+			'name':self.name,
+			'email':user.email
+
+		}
+
 class Task(me.Document):
 	description = me.StringField()
 	deadline = me.DateTimeField()
@@ -15,6 +23,17 @@ class Task(me.Document):
 	user = me.ReferenceField(User)
 	color = me.StringField()
 
+	def to_dict(self):
+		return{
+		'id':str(self.user.id),
+		'title':self.title,
+		'description':self.description,
+		'deadline':int(self.deadline.timestamp()),
+		'added':int(self.added.timestamp()),
+		'color':self.color,
+		'finished':self.finished,
+		'user':str(self.user)
+		}
 
 #Instancia um objeto do Flask
 app = Flask(__name__)
@@ -26,14 +45,49 @@ def  get_users():
 	users = User.objects.all()
 	array = []
 	for user in users:
-		array.append({
-			'id':user._id,
-			'name':user.name,
-			'email':user.email
-			})
+		array.append(user.to_dict())
 	return jsonify(array)
+
+@app.route('/users',methods=['POST'])
+def create_user():
+	if not request.is_json:
+		return jsonify({'error':'not_json'}),400
+	data=request.get_json()
+	name=data.get('name')
+	email=data.get('email')
+	user = User(name=name,email=email)
+	user.save()
+	return jsonify(user.to_dict())
+
+@app.route('/tasks',methods=['POST'])
+def create_task():
+	if not request.is_json:
+		return jsonify({'error':'not_json'}),400
+	data=request.get_json()
+	task = Task(finished=False,added=datetime.now())
+	task.title=data.get('title')
+	task.description = data.get('description')
+	task.deadline=datetime.fromtimestamp(data.get('deadline',0))
+	task.color=data.get('color')
+	task.tags=data.get('tags',[])
+	if 'user' in data.keys():
+		task.user=User.objects.filter(id=data.get('user')).first()
+	task.save()
+	return jsonify(user.to_dict())
+
+@app.route('/tasks/<string:id>',methods=['PATCH'])
+def update_tasks(task_id):
+	if not request.is_json:
+		return jsonify({'error':'not_json'}),400
+	task = Task.objects.filter(id=task_id)
+	if not task:
+		return jsonify({'error':'not_json'}),400
+	data=request.get_json()
+	task.finished=data.get('finished',task.finished)
+	task.save()
+	return jsonify(user.to_dict())
 
 #Roda seu servidor Web
 if __name__ == "__main__":
-	app.run()
+	app.run(port=5001, debug=True)
 
